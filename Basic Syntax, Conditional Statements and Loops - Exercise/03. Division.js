84c1e55dba786b30ed5d900e8b7133e3526c26ef
PeykoGeorgiev <114015932+PeykoGeorgiev@users.noreply.github.com>
2022-12-18 10:31:29 +0100

    Create 03. Division.js

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/03. Division.js
@@ -0,0 +1,23 @@
+function division (num){
+    let devisor = 0;
+    if(num % 10 === 0){
+        devisor = 10;
+    }else if(num % 7 === 0){
+        devisor = 7;
+    }else if(num % 6 === 0){
+        devisor = 6;
+    }else if (num % 3 === 0){
+        devisor = 3;
+    }else if (num % 2 === 0){
+        devisor = 2;
+    }else{
+        console.log("Not divisible");
+    }
+    if(devisor !== 0){
+        console.log(`The number is divisible by ${devisor}`);
+    }
+    } 
+    division(30);
+division(15);
+division(12);
+division(1643);
