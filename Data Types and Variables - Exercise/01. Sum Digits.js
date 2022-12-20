ebce3b45ee375c58d1b9da90078937dce9db8dfb
PeykoGeorgiev <peyko92gm@gmail.com>
2022-12-20 11:05:21 +0100

    Create 01. Sum Digits.js
    Add solution on task from Data Types and Variables - Exercise

---
+++ Data Types and Variables - Exercise/01. Sum Digits.js
@@ -0,0 +1,27 @@
+// function sumDigits (number){
+//     let strArray = number.toString().split('');
+
+
+// let sum = 0
+    
+
+// for(let i = 0; i < strArray.length;i++){
+//   sum  += Number(strArray[i])
+// }
+// console.log(sum);
+// }
+// sumDigits (245678)
+
+function sumDigits (number){
+let numberAsString = number.toString(); // or number + '';
+let sumOfDigits = 0;
+let numberOfDigits = numberAsString.length
+for(let i = 0; i < numberOfDigits; i++){
+  let currentDigit = Number(numberAsString[i]);
+  sumOfDigits += currentDigit;
+  
+
+}
+console.log(sumOfDigits);
+}
+sumDigits (245678)
