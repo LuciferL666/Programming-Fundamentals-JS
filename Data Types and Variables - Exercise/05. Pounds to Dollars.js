2d99c9277b12eff1414fc4a1665714196fc72e58
PeykoGeorgiev <peyko92gm@gmail.com>
2022-12-20 11:12:19 +0100

    Create 05. Pounds to Dollars.js

---
+++ Data Types and Variables - Exercise/05. Pounds to Dollars.js
@@ -0,0 +1,15 @@
+function poundsToDollars (pounds){
+let dollar = pounds * 1.31;
+console.log(`${dollar.toFixed(3)}`);
+}
+poundsToDollars (80)
+
+//another solution
+// function poundsToDollars (pounds){
+//     const poundValue = 1.31
+//     let poundsToDollars = pounds * 1.31; 
+    
+//     console.log(poundsToDollars.toFixed(3));
+//     }
+//     poundsToDollars (80)
+
