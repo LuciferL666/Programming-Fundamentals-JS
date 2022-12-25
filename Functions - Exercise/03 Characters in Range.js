d5b5da3b58d1d87c5402300ff98028ed7a18eee9
PeykoGeorgiev <peyko92gm@gmail.com>
2022-12-25 22:43:43 +0100

    Create 03 Characters in Range.js

---
+++ Functions - Exercise/03 Characters in Range.js
@@ -0,0 +1,37 @@
+function charactersInRange(firstChar, secondChar){
+
+    let rangeStart = Math.min(firstChar.charCodeAt(),secondChar.charCodeAt());
+    let rangeEnd = Math.max(firstChar.charCodeAt(), secondChar.charCodeAt());
+    let resultString = '';
+
+    for(let currentChar = rangeStart + 1; currentChar < rangeEnd; currentChar++){
+        let singleChar = String.fromCharCode(currentChar);
+        
+        if(currentChar + 1 === rangeEnd){
+        resultString += `${singleChar}`;
+        }else{
+            resultString += `${singleChar} `;
+        }
+        
+    }
+    console.log(resultString);
+
+}
+charactersInRange('#',':')
+
+
+// function charactersInRange(firstChar, secondChar){
+
+//     let rangeStart = Math.min(firstChar.charCodeAt(),secondChar.charCodeAt());
+//     let rangeEnd = Math.max(firstChar.charCodeAt(), secondChar.charCodeAt());
+//     let charactersArray = [];
+
+//     for(let currentChar = rangeStart + 1; currentChar < rangeEnd; currentChar++){
+ 
+//  let singleChar = String.fromCharCode(currentChar);
+//  charactersArray.push(singleChar);
+ 
+//     }
+//     console.log(charactersArray.join(' '));
+// }
+// charactersInRange('#',':')
