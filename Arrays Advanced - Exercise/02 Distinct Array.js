8531cd57b9ab052befac32e1baa491a6e9566745
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2022-12-29 22:05:46 +0100

    Update 02 Distinct Array.js

--- Arrays Advanced - Exercise/02 Distinct Array.js
+++ Arrays Advanced - Exercise/02 Distinct Array.js
@@ -13,23 +13,22 @@ function distinctArray(array) {
 distinctArray([7, 8, 9, 7, 2, 3,
 
     4, 1, 2])
+//Other solutions
+// function distinctArray(array) {
+//     let uniqueNumbersArray = new Set(array);
+//     console.log(...uniqueNumbersArray);
+// }
+// distinctArray([7, 8, 9, 7, 2, 3,
 
-console.log("=======");
-function distinctArray(array) {
-    let uniqueNumbersArray = new Set(array);
-    console.log(...uniqueNumbersArray);
-}
-distinctArray([7, 8, 9, 7, 2, 3,
+//     4, 1, 2])
 
-    4, 1, 2])
+// function distinctArray(array) {
 
-function distinctArray(array) {
+//     let numbers = array.filter((value, index, arr) => {
+//         return arr.indexOf(value) === index
+//     });
+//     console.log(numbers.join(' '));
+// }
+// distinctArray([7, 8, 9, 7, 2, 3,
 
-    let numbers = array.filter((value, index, arr) => {
-        return arr.indexOf(value) === index
-    });
-    console.log(numbers.join(' '));
-}
-distinctArray([7, 8, 9, 7, 2, 3,
-
-    4, 1, 2])
+//     4, 1, 2])
