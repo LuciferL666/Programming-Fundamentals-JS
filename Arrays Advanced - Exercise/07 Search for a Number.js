2cefd1a9c1c15334be3551d8a9441da3cd0fb9ee
PeykoGeorgiev <peyko92gm@gmail.com>
2022-12-29 22:00:09 +0100

    Create 07 Search for a Number.js

---
+++ Arrays Advanced - Exercise/07 Search for a Number.js
@@ -0,0 +1,24 @@
+function searchForANumber (nums, actions){
+
+let numbersToTake = actions[0];
+let numbersToDelete = actions[1];
+let specialNumber = actions[2];
+let takenNumbers = nums.slice(0, numbersToTake)
+takenNumbers.splice(0, numbersToDelete);
+
+let counter = 0;
+
+for(let number of takenNumbers){
+    if(number === specialNumber){
+        counter++
+    }
+}
+console.log(`Number ${specialNumber} occurs ${counter} times.`);
+}
+searchForANumber ([5, 2, 3, 4, 1, 6],
+
+    [5, 2, 3])
+
+
+
+
