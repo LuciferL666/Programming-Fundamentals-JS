a04a30de75f060df5b93d6255820cc385ecd5e8c
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2022-12-30 22:25:25 +0100

    Update 04 Convert to JSON.js

--- Objects and Classes - Lab/04 Convert to JSON.js
+++ Objects and Classes - Lab/04 Convert to JSON.js
@@ -1,12 +1,3 @@
-// function convert(data){
-//     let info = JSON.parse(JSON.stringify(data));
-
-//     for(const key of Object.keys(info)){
-//         console.log(`${key}: ${info[key]}`);
-//     }
-// }
-// convert({"name": "George", "age": 40, "town": "Sofia"})
-
 function convertFromJson (name, lastName, hairColor){
     let info = {
         name: name,
