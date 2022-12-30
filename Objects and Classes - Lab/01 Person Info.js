afc3b26f0876546271131e1560d0dd8637035051
PeykoGeorgiev <peyko92gm@gmail.com>
2022-12-30 22:13:40 +0100

    Create 01 Person Info.js

---
+++ Objects and Classes - Lab/01 Person Info.js
@@ -0,0 +1,30 @@
+
+// function printInfo(firstName, lastName, age){
+// let info = {
+// firstName: firstName,
+// lastName: lastName,
+// age: age,
+// }
+// console.log(info);
+
+// return info;
+// }
+// printInfo ("Peter",
+
+// "Pan",
+
+// "20")
+
+function personInformation(firstName, lastName, age){
+let information = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age
+}
+return information;
+}
+personInformation("Peter",
+
+"Pan",
+
+"20")
