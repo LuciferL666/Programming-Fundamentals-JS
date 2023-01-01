e98f5c2f012ea61623b85550319e5566e752f2c1
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-01-01 23:26:47 +0100

    Update 02 Towns.js
    Add two more solutions

--- Objects and Classes - Exercise/02 Towns.js
+++ Objects and Classes - Exercise/02 Towns.js
@@ -1,22 +1,20 @@
-// function towns (array){
-//     let listOfTowns = {}
-// for(let elements of array){
-//     let townInfo = elements.split(' | ')
+function towns (array){
+    let listOfTowns = {}
+for(let elements of array){
+    let townInfo = elements.split(' | ')
 
-//     listOfTowns.town = townInfo[0]
-// listOfTowns.latitude = Number(townInfo[1]).toFixed(2)
-// listOfTowns.longitude = Number(townInfo[2]).toFixed(2)
-// console.log(listOfTowns);
-// }
+    listOfTowns.town = townInfo[0]
+listOfTowns.latitude = Number(townInfo[1]).toFixed(2)
+listOfTowns.longitude = Number(townInfo[2]).toFixed(2)
+console.log(listOfTowns);
+}
+}
+towns (['Sofia | 42.696552 | 23.32601',
 
-
-// }
-// towns (['Sofia | 42.696552 | 23.32601',
-
-// 'Beijing | 39.913818 | 116.363625'])
+'Beijing | 39.913818 | 116.363625'])
 
 
-
+//other solution
 // function towns (list){
 //     let townInfo = {}
 
@@ -36,26 +34,24 @@
 //         console.log(townInfo);
 //     }
 // }
-
-
-
 // towns (['Sofia | 42.696552 | 23.32601',
 
 // 'Beijing | 39.913818 | 116.363625'])
 
-function towns (input){
-let townInfo = {}
+//other solution
+// function towns (input){
+// let townInfo = {}
 
-for (const town of input) {
-    let [townName, latitude, longitude] = town.split(' | ')
-        townInfo.name = townName
-    townInfo.latitude = Number(latitude).toFixed(2)
-    townInfo.longitude = Number(longitude).toFixed(2)
+// for (const town of input) {
+//     let [townName, latitude, longitude] = town.split(' | ')
+//         townInfo.town = townName
+//     townInfo.latitude = Number(latitude).toFixed(2)
+//     townInfo.longitude = Number(longitude).toFixed(2)
 
-    console.log(townInfo);
-}
+//     console.log(townInfo);
+// }
 
-}
-towns (['Sofia | 42.696552 | 23.32601',
+// }
+// towns (['Sofia | 42.696552 | 23.32601',
 
-'Beijing | 39.913818 | 116.363625'])
+// 'Beijing | 39.913818 | 116.363625'])
