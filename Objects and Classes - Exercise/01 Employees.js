d749d3d423dad8b73724133f1800c9cc267b94f6
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-01-01 22:41:56 +0100

    Update 01 Employees.js
    Repair

--- Objects and Classes - Exercise/01 Employees.js
+++ Objects and Classes - Exercise/01 Employees.js
@@ -1,50 +1,3 @@
-// function employees(input){
-// let listOfNames = {}
-
-// for(let employee of input){
-//     listOfNames.name = employee
-//     listOfNames.personalNumber = employee.length
-//     console.log(`Name: ${listOfNames.name} -- Personal Number: ${listOfNames.personalNumber}`);
-// }
-
-
-// }
-// employees ([
-
-//     'Silas Butler',
-    
-//     'Adnaan Buckley',
-    
-//     'Juan Peterson',
-    
-//     'Brendan Villarreal'
-    
-//     ])
-
-
-//     function employees(list){
-//         let employeeList = {}
-
-//        list.forEach(person => {
-//             employeeList[person] = person.length
-//         });
-// for (const key in employeeList) {
-//     console.log(key);
-        
-//     }
-// }
-//     employees ([
-
-//     'Silas Butler',
-    
-//     'Adnaan Buckley',
-    
-//     'Juan Peterson',
-    
-//     'Brendan Villarreal'
-    
-//     ])
-
     function employees (input){
 let listOfNames = {}
 
@@ -53,17 +6,10 @@ for (let employee of input) {
     listOfNames.personalNumber = employee.length;
     console.log(`Name: ${listOfNames.name} -- Personal Number: ${listOfNames.personalNumber}`);
 }
-
-
     }
     employees ([
-
         'Silas Butler',
-        
-        'Adnaan Buckley',
-        
+        'Adnaan Buckley',  
         'Juan Peterson',
-        
         'Brendan Villarreal'
-        
         ])
