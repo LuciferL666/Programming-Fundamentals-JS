0ace31fc4dd4e636fcc8c2d7fb4bff9a0cac6da9
PeykoGeorgiev <peyko92gm@gmail.com>
2023-01-04 23:51:33 +0100

    Create 01 Phone Book.js

---
+++ Associative Arrays - Lab/01 Phone Book.js
@@ -0,0 +1,42 @@
+function phoneBook(input){
+let phoneBook = {};
+
+for(let entry of input){
+    let entryArr = entry.split(' ');
+    let name = entryArr[0];
+    let phone = entryArr[1];
+
+    phoneBook[name] = phone
+}
+for(let name in phoneBook){
+    console.log(`${name} -> ${phoneBook[name]}`);
+}
+}
+phoneBook (['Tim 0834212554',
+
+'Peter 0877547887',
+
+'Bill 0896543112',
+
+'Tim 0876566344'])
+
+function phoneBook (input){
+let phoneBook = {}
+
+for (const entry of input) {
+    let [name, phone] = entry.split(' ')
+    
+    phoneBook[name] = phone
+}
+for (const name in phoneBook) {
+    console.log(`${name} -> ${phoneBook[name]}`);
+        
+    }
+}
+phoneBook (['Tim 0834212554',
+
+'Peter 0877547887',
+
+'Bill 0896543112',
+
+'Tim 0876566344'])
