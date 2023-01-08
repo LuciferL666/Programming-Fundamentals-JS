5fcfc0134db086d908de6c76498de5872926ab82
PeykoGeorgiev <peyko92gm@gmail.com>
2023-01-08 21:37:53 +0100

    Create 06 Pascal Case Splitter.js

---
+++ Text Processing - Exercise/06 Pascal Case Splitter.js
@@ -0,0 +1,37 @@
+function pascalCaseSplitter (text){
+    let split = text.split(/(?=[A-Z])/);
+    console.log(split.join(', '));
+    
+}
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
+
+// function pascalCaseSplitter (text){
+//     let result = text[0];
+// for (let index = 1; index < text.length; index++) {
+//     let current = text[index]
+//     if(current !== current.toUpperCase()){
+//     result += current
+//     }else{
+//         result += `, ${current}`
+//     }
+// }
+// console.log(result);
+// }
+// pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
+
+// function pascalCaseSplitter (text){
+//    let result = text[0]
+// for (const current of text) {
+//     if(current !== current.toUpperCase()){
+//     result += current
+// }else{
+//         result += `, ${current}`
+//     }
+// }
+// let firstComma = result.indexOf(',')
+// let lastLetter = result.substring(0, firstComma)
+// let last = result.substring(firstComma+1)
+
+// console.log(last.trim());
+// }
+// pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
