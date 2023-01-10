a616cfbdc40157e1570a740f300403ce7818f3e3
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-01-10 23:14:17 +0100

    Update 03 SoftUni Bar Income.js
    Repaired

--- Regular Expressions - Exercise/03 SoftUni Bar Income.js
+++ Regular Expressions - Exercise/03 SoftUni Bar Income.js
@@ -1,39 +1,3 @@
-// function softUniBarIncome(customerArray) {
-//     let pattern = /%(?<name>[A-Z][a-z]+)%[^|%$.]*<(?<product>[A-Z][a-z]+)>[^|%$.]*\|(?<count>[0-9])\|[^0-9\|%$.]*(?<price>[0-9|\.]+)\$/gm;
-
-//     let totalIncome = 0;
-//     command = customerArray.shift();
-//     while (command !== "end of shift") {
-//         let currentData = pattern.exec(command);
-
-//         if (currentData) {
-//             let currentCustomer = currentData.groups["name"];
-//             let currentProduct = currentData.groups["product"];
-//             let currentCount = Number(currentData.groups["count"]);
-//             let currentPrice = Number(currentData.groups["price"]);
-
-//             let totalProductPrice = currentCount * currentPrice;
-
-//             console.log(`${currentCustomer}: ${currentProduct} - ${totalProductPrice.toFixed(2)}`);
-//             totalIncome += totalProductPrice;
-//             command = customerArray.shift();
-//             currentData = pattern.exec(command);
-//         }else{
-//             command = customerArray.shift();
-//         }
-//     }
-//     console.log(`Total income: ${totalIncome.toFixed(2)}`);
-// }
-// softUniBarIncome([
-//     "%George%<Croissant>|2|10.3$",
-
-//     "%Peter%<Gum>|1|1.3$",
-
-//     "%Maria%<Cola>|1|2.4$",
-
-//     "end of shift",
-// ]);
-
 function softUniBarIncome(customerArray){
 let pattern = /%(?<name>[A-Z][a-z]+)%[^|%$.]*<(?<product>\w+)>[^|%$.]*\|(?<count>[0-9])\|[^0-9\|%$.]*(?<price>[0-9|\.]+)\$/g;
 
