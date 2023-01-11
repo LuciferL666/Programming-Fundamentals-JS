dc1329e891120b357e3bba698fea0241664eb70b
PeykoGeorgiev <peyko92gm@gmail.com>
2023-01-11 21:28:18 +0100

    Create 02. The Lift.js

---
+++ 01. Programming Fundamentals Mid Exam Retake/02. The Lift.js
@@ -0,0 +1,34 @@
+function problem2TheLift (input){
+    let peopleInQueue = Number(input.shift());
+    let wagons = input.toString().split(' ').map(Number);
+  
+    for(let i = 0; i < wagons.length; i++){
+      if(wagons[i] < 4){
+        if(peopleInQueue >= 4 - wagons[i]){
+          peopleInQueue -= 4 - wagons[i];
+          wagons[i] = 4;
+        }else{
+          wagons[i] += peopleInQueue
+          peopleInQueue = 0
+        }
+      }
+    }
+    let isNotFull = false;
+  
+    for (let i = 0; i < wagons.length; i++) {
+     
+      if(wagons[i] < 4){
+        isNotFull = true
+      }
+      
+    }
+    if(!isNotFull && peopleInQueue > 0){
+      console.log(`There isn't enough space! ${peopleInQueue} people in a queue!`);
+    }
+    if(isNotFull && peopleInQueue == 0){
+      console.log('The lift has empty spots!');
+    }
+    console.log(wagons.join(' '));
+  }
+  problem2TheLift(["15", "0 0 0 0 0"])
+  problem2TheLift([ "20", "0 2 0"])
