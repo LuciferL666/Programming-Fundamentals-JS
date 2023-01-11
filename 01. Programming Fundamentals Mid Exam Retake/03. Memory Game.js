8ee512a9c978094f4671a0613d1018a9070d5b17
PeykoGeorgiev <peyko92gm@gmail.com>
2023-01-11 21:28:35 +0100

    Create 03. Memory Game.js

---
+++ 01. Programming Fundamentals Mid Exam Retake/03. Memory Game.js
@@ -0,0 +1,27 @@
+function memoryGame(arr) {
+    let sequence = arr.shift().split(" ");
+    let moves = 0;
+    while (arr[0] !== "end") {
+        moves++;
+        let [index1, index2] = arr.shift().split(" ").map(Number);
+        if (index1 === index2 || sequence[index1] === undefined || sequence[index2] === undefined) {
+            sequence.splice(sequence.length / 2, 0, `-${moves}a`, `-${moves}a`);
+            console.log("Invalid input! Adding additional elements to the board");
+        } else {
+            if (sequence[index1] === sequence[index2]) {
+                console.log(`Congrats! You have found matching elements - ${sequence[index1]}!`);
+                sequence.splice(Math.max(index1, index2), 1);
+                sequence.splice(Math.min(index1, index2), 1);
+            } else {
+                console.log("Try again!");
+            }
+        }
+        if (sequence.length === 0) {
+            return console.log(`You have won in ${moves} turns!`);
+        }
+    }
+    console.log("Sorry you lose :(");
+    console.log(sequence.join(" "));
+}
+memoryGame( ["1 1 2 2 3 3 4 4 5 5", "1 0","-1 0","1 0", "1 0", "1 0", "end"])
+//memoryGame(["a 2 4 a 2 4", "0 3", "0 2","0 1","0 1", "end"])
