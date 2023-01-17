3ff95bf429ee595b32519d57db6a6b416af226d0
PeykoGeorgiev <peyko92gm@gmail.com>
2023-01-17 22:46:55 +0100

    Create 02. Destination Mapper.js

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper.js
@@ -0,0 +1,16 @@
+function destinationMapper(input) {
+  let pattern = /([=|\/])(?<name>[A-Z][A-Za-z]{2,})\1/g;
+  let match = pattern.exec(input);
+  let output = [];
+  let travelPoints = 0;
+  while (match !== null) {
+    output.push(match.groups.name);
+    travelPoints += match.groups.name.length;
+    match = pattern.exec(input);
+  }
+  console.log(`Destinations: ${output.join(", ")}`);
+  console.log(`Travel Points: ${travelPoints}`);
+}
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=")
+// //
+//destinationMapper("ThisIs some InvalidInput")
