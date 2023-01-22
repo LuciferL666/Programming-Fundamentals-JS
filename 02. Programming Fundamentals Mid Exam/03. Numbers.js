8b63875dbf5fbe50983e46d2fd9ac2b8e67ebf14
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-01-22 18:44:29 +0100

    Update 03. Numbers.js

--- 02. Programming Fundamentals Mid Exam/03. Numbers.js
+++ 02. Programming Fundamentals Mid Exam/03. Numbers.js
@@ -23,6 +23,6 @@ function numbers (input){
         console.log(top5.join(' '));
     }
 numbers('10 20 30 40 50')
-numbers('5 2 3 4 -10 30 40 50 20 50 60 60 51')
+// numbers('5 2 3 4 -10 30 40 50 20 50 60 60 51')
 //numbers('1')
 // numbers('-1 -2 -3 -4 -5 -6')
