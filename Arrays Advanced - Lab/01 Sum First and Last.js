8f6dc4d2456827005d47edc995b52ff1bc5b4378
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-02-06 23:53:01 +0100

    Update 01 Sum First and Last.js

--- Arrays Advanced - Lab/01 Sum First and Last.js
+++ Arrays Advanced - Lab/01 Sum First and Last.js
@@ -1,4 +1,4 @@
- function sumFirstAndLast(numbers){  
+ function sumFirstAndLast(numbers){   
 let firstNumber = Number(numbers[0]);
 let lastNumber = Number(numbers.pop());
 let sum = firstNumber + lastNumber;
