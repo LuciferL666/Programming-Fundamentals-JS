e3433fa88b6735a22fdc3e4b35676ae2f31e93b4
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-02-25 21:21:17 +0100

    Update 01. The Imitation Game.js

--- 01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.js
+++ 01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.js
@@ -1,5 +1,5 @@
 function theImitationGame(input) {
-  let message = input.shift(); 
+  let message = input.shift();
   while (input[0] !== "Decode") {
     let line = input.shift().split("|");
     let command = line[0];
