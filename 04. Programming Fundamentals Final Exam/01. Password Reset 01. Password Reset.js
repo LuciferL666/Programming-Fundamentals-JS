df35a108db804a0b61841126a0dc8703a7b587b4
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-14 12:51:02 +0200

    Create 01. Password Reset 01. Password Reset.js

---
+++ 04. Programming Fundamentals Final Exam/01. Password Reset 01. Password Reset.js
@@ -0,0 +1,39 @@
+function resetPassword(input) {
+  let password = input.shift();
+  while (input[0] !== "Done") {
+    let line = input.shift().split(" ");
+    let command = line[0];
+    if (command === "TakeOdd") {
+      let copy = "";
+      for (let i = 1; i < password.length; i += 2) {
+        copy += password[i];
+      }
+      password = copy;
+      console.log(password);
+    } else if (command === "Cut") {
+      let first = Number(line[1]);
+      let second = Number(line[2]);
+      let start = password.substring(0, first);
+      let end = password.substring(first + second);
+      password = start + end;
+      console.log(password);
+    } else if (command === "Substitute") {
+      let oldSymbol = line[1];
+      let newSymbol = line[2];
+      if (password.includes(oldSymbol)) {
+        password = password.split(oldSymbol).join(newSymbol);
+        console.log(password);
+      } else {
+        console.log(`Nothing to replace!`);
+      }
+    }
+  }
+  console.log(`Your password is: ${password}`);
+}
+resetPassword([
+    "Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr",
+    "TakeOdd",
+    "Cut 15 3",
+    "Substitute :: -",
+    "Substitute | ^",
+    "Done",]);
