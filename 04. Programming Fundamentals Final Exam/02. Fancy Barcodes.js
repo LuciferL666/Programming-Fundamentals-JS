46d03a4afa11cc39d9149c1ad77ada02210b02a3
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-15 23:30:29 +0200

    Create 02. Fancy Barcodes.js

---
+++ 04. Programming Fundamentals Final Exam/02. Fancy Barcodes.js
@@ -0,0 +1,32 @@
+function fancyBarcodes(input) {
+  let n = input.shift();
+
+  for (let barcodes of input) {
+    let pattern = /@#+[A-Z]([A-Za-z0-9]){4,}[A-Z]@#+/g;
+    let match = barcodes.match(pattern);
+    if (match === null) {
+      console.log("Invalid barcode");
+    } else {
+      let barcode = "";
+      for (let letter of match[0]) {
+        if (letter.charCodeAt() >= 48 && letter.charCodeAt() <= 57) {
+          barcode += letter;
+        }
+      }
+      if (barcode.length === 0) {
+        console.log("Product group: 00");
+      } else {
+        console.log(`Product group: ${barcode}`);
+      }
+    }
+  }
+}
+fancyBarcodes([
+    "6",
+    "@###Val1d1teM@###",
+    "@#ValidIteM@#",
+    "##InvaliDiteM##",
+    "@InvalidIteM@",
+    "@#Invalid_IteM@#",
+    "@#ValiditeM@#",
+]);
