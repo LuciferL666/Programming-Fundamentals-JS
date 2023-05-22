c8dfc7e6454ea065b616f7b46ad2a377b5d51d12
Peyko Georgiev <120681638+LuciferL666@users.noreply.github.com>
2023-05-22 23:51:15 +0200

    Update 03. Heroes of Code and Logic VII.js

--- 04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII.js
+++ 04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII.js
@@ -75,3 +75,15 @@ heroes([
     "CastSpell - Kyrre - 15 - ViewEarth",
     "End",
 ]);
+// heroes([
+// '4',
+// 'Adela 90 150',
+// 'SirMullich 70 40',
+// 'Ivor 1 111',
+// 'Tyris 94 61',
+// 'Heal - SirMullich - 50',
+// 'Recharge - Adela - 100',
+// 'CastSpell - Tyris - 1000 - Fireball',
+// 'TakeDamage - Tyris - 99 - Fireball',
+// 'TakeDamage - Ivor - 3 - Mosquito',
+// 'End'])
